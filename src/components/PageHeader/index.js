e6c3e3846a902789d8de254e130fda2dc7e4394e
vinicius.hashimoto <vinicius.hashimoto@getmore.com.br>
2020-09-27 20:18:00 -0300

    page header created

---
+++ src/components/PageHeader/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const pokemonLogo = require('../../assets/images/pokemon_logo.png');
+require('./styles.css');
+const PageHeader = () => {
+  return (
+    <div className='pageHeader'>
+      <Link to='/'>
+        <img src={pokemonLogo} alt='' />
+      </Link>
+    </div>
+  );
+};
+
+export default PageHeader;

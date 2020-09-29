28b6a68de0857d76418d3901fbce68f1c601074c
vinicius.hashimoto <vinicius.hashimoto@getmore.com.br>
2020-09-28 22:20:01 -0300

    update pokedex router

--- src/pages/Pokemon/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Pokemon = () => {
-  return <div />;
-};
-
-export default Pokemon;
--- src/router/Routes.js
+++ src/router/Routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Redirect, Route } from 'react-router';
 import Home from '../pages/Home';
 import Pokedex from '../pages/Pokedex';
-import Pokemon from '../pages/Pokemon';
 
 const Routes = () => {
   return (
@@ -10,7 +9,6 @@ const Routes = () => {
       <Route exact path='/' component={Home} />
       <Route exact path='/home' render={() => <Redirect to='/' />} />
       <Route exact path='/pokedex' component={Pokedex} />
-      <Route exact path='/pokemon:id' component={Pokemon} />
     </>
   );
 };

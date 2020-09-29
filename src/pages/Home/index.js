e1bd58e90b6326b0c5fd0b608e746e993d123ac4
vinicius.hashimoto <vinicius.hashimoto@getmore.com.br>
2020-09-28 22:24:53 -0300

    add some messages about pokeapi

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -26,11 +26,24 @@ const Home = () => {
           },
         }}
       >
-        <CardGeneration generationName={generation.name} />
+        <CardGeneration url={generation.url} generationName={generation.name} />
       </Link>
     ));
 
-  return <div className='generationsCard'>{renderGenerationsCard()}</div>;
+  return (
+    <>
+      <div className='alert alert-danger' role='alert'>
+        Data from last year's Sword and Shield games (generation-viii) is currently being added to PokéAPI.
+        While waiting for an upstream project (Veekun) for verified data we decided to allow our community to
+        bring in new data from various different sources. If you want to participate to this update project{' '}
+        <a href='https://github.com/PokeAPI/pokeapi/issues/520'>read more on GitHub</a>.
+      </div>
+      <div className='alert alert-success' role='alert'>
+        Select the generation to see pokedex of Pokemon Game
+      </div>
+      <div className='generationsCard container'>{renderGenerationsCard()}</div>
+    </>
+  );
 };
 
 export default Home;

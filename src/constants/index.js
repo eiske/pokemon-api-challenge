d5b2bd87f8d79b5b0d1a2d33af817f3859c4478d
vinicius.hashimoto <vinicius.hashimoto@getmore.com.br>
2020-09-29 12:48:50 -0300

    fiz fighting color type

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,7 +4,7 @@ export const TYPE_COLORS = {
   dragon: '755EDF',
   electric: 'FCBC17',
   fairy: 'F4B1F4',
-  fighting: '823551D',
+  fighting: '82355D',
   fire: 'E73B0C',
   flying: 'A3B3F7',
   ghost: '6060B2',

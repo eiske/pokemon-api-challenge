a03c2079e6168677a23a7a47879c7198e33b77e3
vinicius.hashimoto <vinicius.hashimoto@getmore.com.br>
2020-09-28 22:23:04 -0300

    pokemon showing in pokedex

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Dialog, DialogActions } from '@material-ui/core';
+import Pokemon from '../Pokemon';
 
 require('./style.css');
 const PokemonCard = ({ name, url }) => {
   const [pokeName, setPokeName] = useState();
   const [imageUrl, setimageUrl] = useState();
   const [pokeIndex, setpokeIndex] = useState();
+  const [dialog, setDialog] = useState(false);
+
   useEffect(() => {
     const pokemonIndex = url.split('/')[url.split('/').length - 2];
     const imageURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeIndex}.png`;
@@ -14,16 +17,32 @@ const PokemonCard = ({ name, url }) => {
     setpokeIndex(pokemonIndex);
   }, [url, name, pokeIndex]);
 
+  const handleOpenDialog = () => {
+    setDialog(true);
+  };
+
+  const handleClose = () => {
+    setDialog(false);
+  };
+
   return (
-    <Link to={`/pokemon/${pokeIndex}`}>
-      <div className='card shadow'>
-        <h5 className='card-header'>{pokeIndex}</h5>
+    <>
+      <div onClick={handleOpenDialog} className='shadow card'>
+        <h5 className='card-header'>#{pokeIndex}</h5>
         <img src={imageUrl} alt='' style={{ width: '100%', height: '100%' }} className='mx-auto mt-2' />
         <div className='card-body mx-auto my-auto'>
           <h6 className='card-title'>{pokeName}</h6>
         </div>
       </div>
-    </Link>
+      <Dialog onClose={handleClose} open={dialog}>
+        <Pokemon pokemonId={pokeIndex} />
+        <DialogActions>
+          <button type='button' class='btn btn-outline-danger' onClick={handleClose} color='primary'>
+            Close
+          </button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
